aacdfbf5ba16cb0fb2a3f4d6015f6f347e1c27fd
Somayeh Ebrahimzadeh <ebrahimzadeh3394@gmail.com>
2022-07-24 11:40:34 +0430

    separate device file

--- components/uikit/table/TableStyle.js
+++ components/uikit/table/TableStyle.js
@@ -1,25 +1,5 @@
 import styled from "styled-components";
-
-const size = {
-  mobileS: "320px",
-  mobileM: "375px",
-  mobileL: "425px",
-  tablet: "768px",
-  laptop: "1024px",
-  laptopL: "1440px",
-  desktop: "2560px",
-};
-
-export const device = {
-  mobileS: `(max-width: ${size.mobileS})`,
-  mobileM: `(max-width: ${size.mobileM})`,
-  mobileL: `(max-width: ${size.mobileL})`,
-  tablet: `(max-width: ${size.tablet})`,
-  laptop: `(max-width: ${size.laptop})`,
-  laptopL: `(max-width: ${size.laptopL})`,
-  desktop: `(max-width: ${size.desktop})`,
-  desktopL: `(max-width: ${size.desktop})`,
-};
+import { device } from "./device";
 
 export const TableWrapper = styled.table`
   border: 1px solid #ddd;
